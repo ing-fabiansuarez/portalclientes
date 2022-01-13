552de2f5e8352b11e44e17d9bf5ff36a4007d446
Fabian Suarez <fsuarez120@unab.edu.co>
2022-01-13 14:40:26 -0500

    Se habilito subir la foto de perfil de una persona o usuario del sistema

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/', function () {
+        return view('home');
+    })->name('home_system');
+    
+    /* Route::get('/', function () {
+        return view('welcome');
+    }); */
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
     
-    Route::get('/home', function () {
-        return view('home');
-    })->name('home_system');
 });
