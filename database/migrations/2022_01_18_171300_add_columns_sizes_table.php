925f81582c929974879dfc7aba13267359bc16c1
Fabian Suarez <fsuarez120@unab.edu.co>
2022-01-18 12:17:40 -0500

    se creo la migracion de las sizes de cada producto

---
+++ database/migrations/2022_01_18_171300_add_columns_sizes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsSizesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sizes', function (Blueprint $table) {
+            $table->unsignedBigInteger('product_id')
+                ->after('id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
