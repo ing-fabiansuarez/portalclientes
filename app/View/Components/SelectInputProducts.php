4a74562d13d4c2d2b1271b7612c74860e50b7fe7
Fabian Suarez <fsuarez120@unab.edu.co>
2022-01-22 10:47:55 -0500

    avances del componente livewire para mostrar las referencias para guardar en la base de datos

---
+++ app/View/Components/SelectInputProducts.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Product;
+use Illuminate\View\Component;
+
+class SelectInputProducts extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        $produts = Product::where('active', true)->orderBy('score','desc')->get();
+        return view('components.select-input-products', compact('produts'));
+    }
+}
--- resources/views/livewire/references/show-references-table.blade.php
+++ resources/views/livewire/references/show-references-table.blade.php
@@ -5,44 +5,79 @@
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                            <table class="min-w-full divide-y divide-gray-200">
-                                <thead class="bg-gray-50">
-                                    <tr>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                            Nombre de la referencia
-                                        </th>
-                                        <th scope="col" class="relative px-6 py-3">
-                                            <span class="sr-only">Edit</span>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody class="bg-white divide-y divide-gray-200">
-                                    @foreach ($references as $ref)
+
+                            <div class="px-6 pt-4">
+                                <form class="w-full max-w-sm">
+                                    <div class="md:flex md:items-center">
+                                        <div class="md:w-1/3">
+                                            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                                for="inline-full-name">
+                                                Producto
+                                            </label>
+                                        </div>
+                                        <div class="md:w-2/3">
+                                            <x-select-input-products wire:model="idProduct" />
+                                        </div>
+                                    </div>
+                                    <div class="md:flex md:items-center">
+                                        <div class="md:w-1/3">
+                                            <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                                for="inline-full-name">
+                                                Referencia o Nombre
+                                            </label>
+                                        </div>
+                                        <div class="md:w-2/3">
+                                            <x-jet-input type="text" wire:model="ref" class="w-full"/>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            @if ($references->count())
+                                <table class="min-w-full divide-y divide-gray-200">
+                                    <thead class="bg-gray-50">
                                         <tr>
-                                            <td class="px-6 py-4 whitespace-nowrap">
-                                                <div class="flex items-center">
-                                                    <div class="flex-shrink-0 h-10 w-10">
-                                                        <img class="h-10 w-10 rounded-full"
-                                                            src="{{ Storage::url($ref->image->url) }}" alt="">
-                                                    </div>
-                                                    <div class="ml-4">
-                                                        <div class="text-sm font-medium text-gray-900">
-                                                            Ref. {{ $ref->num_ref . ' - ' . $ref->name_ref }}
-                                                        </div>
-                                                        <div class="text-sm text-gray-500">
-                                                            {{ $ref->product->name_product }}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Agregar</a>
-                                            </td>
+                                            <th scope="col"
+                                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                Referencias
+                                            </th>
+                                            <th scope="col" class="relative px-6 py-3">
+                                                <span class="sr-only">Edit</span>
+                                            </th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                        @foreach ($references as $ref)
+                                            <tr>
+                                                <td class="px-6 py-4 whitespace-nowrap">
+                                                    <div class="flex items-center">
+                                                        <div class="flex-shrink-0 h-10 w-10">
+                                                            <img class="h-10 w-10 rounded-full"
+                                                                src="{{-- {{ Storage::url($ref->image->url) }} --}}" alt="">
+                                                        </div>
+                                                        <div class="ml-4">
+                                                            <div class="text-sm font-medium text-gray-900">
+                                                                Ref. {{ $ref->num_ref . ' - ' . $ref->name_ref }}
+                                                            </div>
+                                                            <div class="text-sm text-gray-500">
+                                                                {{ $ref->product->name_product }}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                    <a href="#"
+                                                        class="text-indigo-600 hover:text-indigo-900">Agregar</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            @else
+                                <div class="mx-4 py-4">
+                                    No existe ning&uacute;n registro coincidente.
+                                </div>
+                            @endif
+
 
                         </div>
                     </div>
