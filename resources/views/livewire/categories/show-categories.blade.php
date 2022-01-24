69c024eb15fb363d0fd17629bb7df7c5516140a3
Fabian Suarez <fsuarez120@unab.edu.co>
2022-01-24 11:05:17 -0500

    diseño de los items en el carrito del pedido

--- resources/views/livewire/categories/show-categories.blade.php
+++ resources/views/livewire/categories/show-categories.blade.php
@@ -3,8 +3,8 @@
         @foreach ($categories as $category)
             <div class="col-span-12 xl:col-span-3 md:col-span-6">
                 <div class="card background-card-fabian bg-primary">
-                    <div class="card-body"><img class="rounded"
-                            src="{{ Storage::url($category->image->url) }}" alt="First slide" />
+                    <div class="card-body">{{-- <img class="rounded"
+                            src="{{ Storage::url(@$category->image->url) }}" alt="First slide" /> --}}
                         <p class="text-xl font-aguacate font-bold mt-4 uppercase">{{ $category->name }} </p>
                         <div class="my-3">
                             <div class="flex justify-between">
--- resources/views/livewire/references/show-references-table.blade.php
+++ resources/views/livewire/references/show-references-table.blade.php
@@ -27,7 +27,7 @@
                                             </label>
                                         </div>
                                         <div class="md:w-2/3">
-                                            <x-jet-input type="text" wire:model="ref" class="w-full"/>
+                                            <x-jet-input type="text" wire:model="ref" class="w-full" />
                                         </div>
                                     </div>
                                 </form>
@@ -52,7 +52,7 @@
                                                     <div class="flex items-center">
                                                         <div class="flex-shrink-0 h-10 w-10">
                                                             <img class="h-10 w-10 rounded-full"
-                                                                src="{{-- {{ Storage::url($ref->image->url) }} --}}" alt="">
+                                                                src="{{ Storage::url(@$ref->image->url) }}" alt="">
                                                         </div>
                                                         <div class="ml-4">
                                                             <div class="text-sm font-medium text-gray-900">
@@ -82,7 +82,39 @@
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
+        <div class="col-span-12 xl:col-span-8 md:col-span-8">
+            <div class="card mb-5">
+                <div class="card-body">
+                    <div class="flex flex-wrap justify-between items-center sm:flex-row"><img
+                            class="w-52 h-28 mr-0 mb-3 sm:mr-4 sm:mb-0" src="assets/images/products/iphone-1.jpg" />
+                        <div class="flex-1 text-center sm:text-left">
+                            <p class="text-lg mb-1 text-center sm:text-left">Portable Speaker with HD
+                                Sound</p>
+                            <div class="flex items-center mb-3 justify-center sm:justify-start">
+                                <p class="mr-1">category: </p><a
+                                    class="text-primary-500 hover:text-primary-500" href="#">Speaker
+                                </a>
+                            </div>
+                            <div class="mb-2"><a class="text-primary-500 mb-1">In Stock </a>
+                            </div>
+                            <div class="inline-flex mr-2 items-center my-4 sm:my-0">
+                                <button class="pr-4"><i class="i-Arrow-Left"></i></button>
+                                <p class="text-base">01</p>
+                                <button class="pl-4"><i class="i-Arrow-Right"></i></button>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <p class="font-semibold text-2xl mb-3">$50</p>
+                            <button class="btn ripple btn-danger-outline mr-2" type="button"> <i
+                                    class="i-Close-Window mr-2 align-middle font-semibold"></i>Remove</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </div>
