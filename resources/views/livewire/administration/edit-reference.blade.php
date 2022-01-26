585c970c1f4fc570043862c2522b141d1ae5978d
ivanveraj <jsjlds66@gmail.com>
2022-01-26 16:46:56 -0500

    Arreglo vista editar referencias

--- resources/views/livewire/administration/edit-reference.blade.php
+++ resources/views/livewire/administration/edit-reference.blade.php
@@ -31,36 +31,42 @@
                 </select>
                 <x-jet-input-error for="reference.product_id" />
             </div>
-            <div class="mb-4">
-                <x-jet-label value="N° de referencia"></x-jet-label>
-                <x-jet-input wire:model="reference.num_ref" type="number" class="w-full" />
-                <x-jet-input-error for="reference.num_ref" />
+
+
+            <div class="mb-4 flex justify-center text-center">
+                <div class="mr-4">
+                    <x-jet-label value="N° de referencia"></x-jet-label>
+                    <x-jet-input wire:model="reference.num_ref" type="number" class="w-full" />
+                    <x-jet-input-error for="reference.num_ref" />
+                </div>
+                <div class="ml-4">
+                    <x-jet-label value="Estado" class="mb-2"></x-jet-label>
+                    <div class="cursor-pointer ml-2">
+                        @if ($reference->active_ref == 0)
+                            <label class="switch">
+                                <input class="CheckBoxState" type="checkbox" data-onstyle="success"
+                                    data-offstyle="danger" data-toggle="toggle" data-on="Active" data-off="InActive"
+                                    wire:model="reference.active_ref">
+                                <span class="slider round ml-3">-Inactivo</span>
+                            </label>
+                        @else
+                            <label class="switch">
+                                <input class="CheckBoxState" type="checkbox" data-onstyle="success"
+                                    data-offstyle="danger" data-toggle="toggle" data-on="Active" data-off="InActive"
+                                    wire:model="reference.active_ref" checked>
+                                <span class="slider round ml-3">-Activo</span>
+                            </label>
+                        @endif
+                    </div>
+                </div>
             </div>
+
             <div class="mb-4">
                 <x-jet-label value="Nombre de la referencia"></x-jet-label>
                 <x-jet-input wire:model="reference.name_ref" type="text" class="w-full" />
                 <x-jet-input-error for="reference.name_ref" />
             </div>
-            <div class="mb-4">
-                <x-jet-label value="Estado" class="mb-2"></x-jet-label>
-                <div class="cursor-pointer ml-2">
-                    @if ($reference->active_ref == 0)
-                        <label class="switch">
-                            <input class="CheckBoxState" type="checkbox" data-onstyle="success" data-offstyle="danger"
-                                data-toggle="toggle" data-on="Active" data-off="InActive"
-                                wire:model="reference.active_ref">
-                            <span class="slider round ml-3">-Inactivo</span>
-                        </label>
-                    @else
-                        <label class="switch">
-                            <input class="CheckBoxState" type="checkbox" data-onstyle="success" data-offstyle="danger"
-                                data-toggle="toggle" data-on="Active" data-off="InActive"
-                                wire:model="reference.active_ref" checked>
-                            <span class="slider round ml-3">-Activo</span>
-                        </label>
-                    @endif
-                </div>
-            </div>
+
         </x-slot>
         <x-slot name="footer">
             <x-jet-secondary-button wire:click="$set('open',false)">
